cd65a59b5701c6cfc18dc2dee06dbcc5cd6b072d
Boring545 <78906011+Boring545@users.noreply.github.com>
2024-07-09 16:02:09 +0800

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,35 +1,38 @@
 #!/bin/sh
+ROOT_DIR="."
+OUTPUT_DIR_PERF="perf_out"
+OUTPUT_DIR_TEST="test_out"
 
-# 检查参数
+# 删除已有输出结果
+rm -rf "$OUTPUT_DIR_PERF"
+rm -rf "$OUTPUT_DIR_TEST"
 
-
-ROOT_DIR=.
-OUTPUT_DIR_Perf=perf_out
-OUTPUT_DIR_Test=test_out
-#删除已有输出结果
-rm -rf perf
-rm -rf test_r
 # 创建输出目录（如果不存在）
-mkdir -p "$OUTPUT_DIR_Perf"
-mkdir -p "$OUTPUT_DIR_Test"
-# 查找所有 _test.go 文件
+mkdir -p "$OUTPUT_DIR_PERF"
+mkdir -p "$OUTPUT_DIR_TEST"
+
+# 查找所有 _test.go 文件并执行测试
 find "$ROOT_DIR" -name '*_test.go' | while read -r test_file; do
-    echo "Processing file: $test_file"
     # 提取包名
     pkg=$(dirname "$test_file")
-    echo $pkg
+
     # 查找所有测试函数
     grep -oP 'func \K(Test\w*)' "$test_file" | while read -r test_func; do
-        echo $test_func
         # 生成执行命令
         cmd="go test -v -race -run ^$test_func$ $pkg"
         path=$(echo "$pkg" | sed 's:.*/::')
         file="$test_func-$path"
-        $cmd > $file.txt 2>&1
-        echo $test_func >>$file.txt
-        echo "($pkg)" >>$file.txt
-        mv $file.txt $OUTPUT_DIR_Test
+        
+        # 执行测试命令
+        $cmd > "$file.txt" 2>&1
+        
+        # 将测试结果移动到测试输出目录
+        echo $test_func >> "$file.txt"
+        echo "($pkg)" >> "$file.txt"
+        mv "$file.txt" "$OUTPUT_DIR_TEST"
+        
         # 调用 perf 脚本
-        ./performance_counter_920.sh "$cmd" "$OUTPUT_DIR_Perf" 
+        ./performance_counter_920.sh "$cmd" "$OUTPUT_DIR_PERF"
     done
 done
+
